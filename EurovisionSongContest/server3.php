fddff767aeae934cd829d37b2ae26573d8f3b72c
andrijana <mionicandrijana90@hotmail.com>
2013-02-19 13:28:21 +0100

    proba
    proba

--- EurovisionSongContest/server3.php
+++ EurovisionSongContest/server3.php
@@ -1,20 +1,39 @@
 <?php
+//proverava se MIME tip sadrzaja koji zahteva klijent. Ukoliko nije zadat, generise se JSON
+if (isset ($_SERVER['HTTP_ACCEPT'])){
+switch ($_SERVER['HTTP_ACCEPT']){
+case "application/json":
+generisiJSON();
+break;
+case "application/xml":
+generisiXML();
+break;
+default:
+generisiJSON();
+break;
+}
+} else {
+generisiJSON();
+}
 
+function generisiJSON(){
+require "konekcija.php";
 //definiše se mime type
 header("Content-type: application/json");?>{"drzave":<?php
-require_once "konekcija.php";
-// $sql="SELECT drzave.*, predstavnici.ime_prezime FROM drzave, predstavnici WHERE drzave.id_drz=predstavnici.id_drz";
- $sql="SELECT drzave.naziv, predstavnici.ime_prezime, predstavnici.pesma FROM predstavnici INNER JOIN drzave ON predstavnici.id_drz=drzave.id_drz INNER JOIN faze_takmicenja ON drzave.id_faza=faze_takmicenja.id_faza WHERE faze_takmicenja.id_faza='3'";
-//$sql="SELECT drzave.naziv, predstavnici.ime_prezime, predstavnici.pesma FROM drzave, faze_takmicenja WHERE predstavnici.id_drz=drzave.id_drz";
+$sql="SELECT drzave.naziv, predstavnici.ime_prezime, predstavnici.pesma FROM predstavnici INNER JOIN drzave ON predstavnici.id_drz=drzave.id_drz INNER JOIN faze_takmicenja ON drzave.id_faza=faze_takmicenja.id_faza WHERE faze_takmicenja.id_faza='3'";
 if (!$q=$mysqli->query($sql)){
+//ako se upit ne izvrši
 echo '{"greska":"Nastala je greška pri izvršavanju upita."}';
 exit();
 } else {
+//ako je upit u redu
 if ($q->num_rows>0){
+//ako ima rezultata u bazi
 $niz = array();
 while ($red=$q->fetch_object()){
 $niz[] = $red;
 }
+//print_r ($niz);
 $niz_json = json_encode ($niz);
 print ($niz_json);
 } else {
@@ -22,3 +41,69 @@ print ($niz_json);
 echo '{"greska":"Nema rezultata."}';
 }
 }?>}
+<?php
+$mysqli->close();
+}
+
+function generisiXML(){
+//definiše se mime type
+header("Content-type: application/xml");
+//konekcija ka bazi
+require_once "konekcija.php";
+//priprema upita
+ $sql="SELECT drzave.naziv, predstavnici.ime_prezime, predstavnici.pesma FROM predstavnici INNER JOIN drzave ON predstavnici.id_drz=drzave.id_drz INNER JOIN faze_takmicenja ON drzave.id_faza=faze_takmicenja.id_faza WHERE faze_takmicenja.id_faza='3'";
+//kreiranje XMLDOM dokumenta
+$dom = new DomDocument('1.0','utf-8');
+
+//dodaje se koreni element
+ $drzave = $dom->appendChild($dom->createElement('drzave'));
+
+//izvršavanje upita
+if (!$q=$mysqli->query($sql)){
+
+ $greska = $drzave->appendChild($dom->createElement('greska'));
+
+ $greska->appendChild($dom->createTextNode("Došlo je do greške pri izvršavanju upita"));
+} else {
+//ako je upit u redu
+if ($q->num_rows>0){
+//ako ima rezultata u bazi
+$niz = array();
+while ($red=$q->fetch_object()){
+  
+ $drzava = $drzave->appendChild($dom->createElement('drzava'));
+$naziv = $drzava->appendChild($dom->createElement('naziv'));
+
+ $naziv->appendChild($dom->createTextNode($red->naziv));
+
+ $ime_prezime = $drzava->appendChild($dom->createElement('ime_prezime'));
+ 
+ $ime_prezime->appendChild($dom->createTextNode($red->ime_prezime));
+  $pesma = $drzava->appendChild($dom->createElement('pesma'));
+ 
+ $pesma->appendChild($dom->createTextNode($red->pesma));
+}
+} else {
+//ako nema rezultata u bazi
+  //dodaje se element <greska> u korenom elementu <proizvodi>
+ $greska = $tekst->appendChild($dom->createElement('greska'));
+
+ $greska->appendChild($dom->createTextNode("Nema unetih proizvoda"));
+}
+}
+//cuvanje XML-a
+$xml_string = $dom->saveXML(); 
+echo $xml_string;
+//zatvaranje konekcije
+$mysqli->close();
+}
+?>
+
+
+
+
+
+
+
+
+
--- EurovisionSongContest/thecontest.php
+++ EurovisionSongContest/thecontest.php
@@ -14,7 +14,7 @@ include 'include/header.php';
 $url = 'http://localhost/ItehProjekat/EurovisionSongContest/server1.php';
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json'));
 curl_setopt($curl, CURLOPT_POST, false);
 $curl_odgovor = curl_exec($curl);
 curl_close($curl);
@@ -68,7 +68,7 @@ foreach ($json_objekat->drzave as $t){
 $url = 'http://localhost/ItehProjekat/EurovisionSongContest/server2.php';
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json'));
 curl_setopt($curl, CURLOPT_POST, false);
 $curl_odgovor = curl_exec($curl);
 curl_close($curl);
@@ -122,7 +122,7 @@ foreach ($json_objekat->drzave as $t){
 $url = 'http://localhost/ItehProjekat/EurovisionSongContest/server3.php';
 $curl = curl_init($url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-//curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Accept: application/json'));
 curl_setopt($curl, CURLOPT_POST, false);
 $curl_odgovor = curl_exec($curl);
 curl_close($curl);
